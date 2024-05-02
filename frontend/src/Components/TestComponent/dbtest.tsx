57889beea891ed8db6213d151ffa2c3a340aff51
Anssi Peltola <anssipeltola@hotmail.com>
2024-05-02 10:45:58 +0300

    Added frontend files

---
+++ frontend/src/Components/TestComponent/dbtest.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function TestComponent() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:3000/test') // Ensure the URL matches your backend server address and port
+            .then(response => response.json())
+            .then(data => setMessage(data.message))
+            .catch(error => console.error('Error fetching data: ', error));
+    }, []);
+
+    return (
+        <div>
+            <p>Message from backend: {message}</p>
+        </div>
+    );
+}
+
+export default TestComponent;
---
+++ frontend/src/Pages/Login/Login.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/login`, { email, password });
+      console.log(response.data); // Here you would typically handle the logged in user data
+      setLoginStatus('Login successful!'); 
+    } catch (error) {
+      console.error(error);
+      setLoginStatus('Login failed. Please check your email and password.');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <p>{loginStatus}</p>
+    </div>
+  );
+}
+
+export default Login;
