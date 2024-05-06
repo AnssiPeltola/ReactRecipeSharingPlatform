3f1602fd3e8d3573500664cafacd9b67ddb1579a
Anssi Peltola <anssipeltola@hotmail.com>
2024-05-06 14:59:15 +0300

    Toteutettu route ja navigointi
    - Lisätty route ja navigoint käyttämällä react-router-domia.
    - Lisätty NavBar
    - lisätty ErrorPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import TestComponent from './Components/TestComponent/dbtest'; // Adjust the path as necessary
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as ROUTES from '../src/Constants/routes';
+import TestComponent from './Components/TestComponent/dbtest';
 import Register from './Pages/Register/Register';
 import Login from './Pages/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <TestComponent />
-                <Register />
-                <Login />
-            </header>
+      <Router>
+        <div id='root'>
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path={ROUTES.LANDING} element={<TestComponent />} />
+              <Route path={ROUTES.REGISTER} element={<Register />} />
+              <Route path={ROUTES.LOGIN} element={<Login />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </main>
         </div>
+      </Router>
     );
-}
+};
 
 export default App;
